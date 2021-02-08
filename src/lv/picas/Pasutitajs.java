5906dd49e2aef86ccffbd076267c0d277382ced1
Telma Laudobele <telmy2458@hotmail.lv>
2021-02-08 14:28:08 +0200

    Pievieno piegades opciju picas atributiem, pievieno Pasutitaja klasi

---
+++ src/lv/picas/Pasutitajs.java
@@ -0,0 +1,15 @@
+package lv.picas;
+
+public class Pasutitajs {
+	//atributi
+ int telefonaNum; //telefona numurs
+ String vardsUzvards; 
+ String adrese;
+ 
+//konstruktors
+	public Pasutitajs(int telN, String vards, String adr) {
+		this.telefonaNum = telN;
+		this.vardsUzvards = vards;
+		this.adrese = adr;
+	}
+}
--- src/lv/picas/Pica.java
+++ src/lv/picas/Pica.java
@@ -5,12 +5,14 @@ public class Pica {
 	int izmers; //cm diametrs 
 	boolean sieraMalas; //vai malām ir siera pildījums?
 	String piedevas; //izmantošu kā masīvu, lai pievienotu vairākas piedevas!
+	boolean piegade;
 	
 	//konstruktors
-	public Pica(int picasIzmers, String picasPiedevas, boolean picasSiMal) {
-		this.izmers = picasIzmers;
-		this.sieraMalas = picasSiMal;
-		this.piedevas = picasPiedevas;
+	public Pica(int izm, String p, boolean sierM, boolean pieg) {
+		this.izmers = izm;
+		this.sieraMalas = sierM;
+		this.piedevas = p;
+		this.piegade = pieg;
 	}
 
 }
