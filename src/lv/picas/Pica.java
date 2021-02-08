7a7cec73f30b7bfb7613931da7a62e5a57ef6704
Telma Laudobele <telmy2458@hotmail.lv>
2021-02-08 14:12:08 +0200

    Izveidoju picas klases atribūtus bez metodēm

--- src/lv/picas/Pica.java
+++ src/lv/picas/Pica.java
@@ -1,7 +1,16 @@
 package lv.picas;
 //OOP programmesana?
 public class Pica {
-
-
+	//atribūti
+	int izmers; //cm diametrs 
+	boolean sieraMalas; //vai malām ir siera pildījums?
+	String piedevas; //izmantošu kā masīvu, lai pievienotu vairākas piedevas!
+	
+	//konstruktors
+	public Pica(int picasIzmers, String picasPiedevas, boolean picasSiMal) {
+		this.izmers = picasIzmers;
+		this.sieraMalas = picasSiMal;
+		this.piedevas = picasPiedevas;
+	}
 
 }
