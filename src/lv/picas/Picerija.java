3b6791b3e85f9805650eb5a26c4fc1ad26572d21
Telma Laudobele <telmy2458@hotmail.lv>
2021-02-08 19:05:19 +0200

    Opcija apstādināt programmu ar uzlecošu lodziņu kas brīdina lietotāju

--- src/lv/picas/Picerija.java
+++ src/lv/picas/Picerija.java
@@ -5,10 +5,10 @@ import javax.swing.JOptionPane;
 public class Picerija {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		   String[] options = {"Jauns sūtījums", "Aktīvie sūtījumi", "Iziet no programmas"};
+		int x;
+		 do {  String[] options = {"Jauns sūtījums", "Aktīvie sūtījumi", "Iziet no programmas"};
 	      
-	        int x = JOptionPane.showOptionDialog(null, "Veic izvēli!",
+	        x = JOptionPane.showOptionDialog(null, "Veic izvēli!",
 	                null,
 	                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
 	     switch(x) {
@@ -16,19 +16,20 @@ public class Picerija {
 			
 	     case 1: JOptionPane.showMessageDialog(null, "Nav izstrādāts kods...");
 	    	 
-	     case 2: System.exit(0);
+	     case 2: JOptionPane.showMessageDialog(null,"Programma tiek apturēta","WARNING", JOptionPane.WARNING_MESSAGE);
 default: 
 	    }
+		 }while(x != 2);
 		
 	}
 	public static void jaunsSutijums(){
 		int telNumurs;
 		String vardsUzv, dzivvieta, y;
+		vardsUzv = JOptionPane.showInputDialog("Ieraksti vārdu un uzvārdu!");
 		do {
 			y = JOptionPane.showInputDialog("Ieraksti telefona numuru!");
 		}while(y.length() != 8);
 		telNumurs = Integer.parseInt(y);
-		vardsUzv = JOptionPane.showInputDialog("Ieraksti vārdu un uzvārdu!");
 		dzivvieta = JOptionPane.showInputDialog("Ieraksti adresi");
 		Pasutitajs sutitajs = new Pasutitajs(telNumurs, vardsUzv, dzivvieta);
 		sutitajs.pasutitajaDati();
