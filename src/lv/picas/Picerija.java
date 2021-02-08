2e5c0989a2b342e004a1e57fc4123acb1d40b5a5
Telma Laudobele <telmy2458@hotmail.lv>
2021-02-08 18:35:08 +0200

    Pievienoju telefona numura pārbaudi (vai ir 8 cipari), pievienoju mērču opciju

--- src/lv/picas/Picerija.java
+++ src/lv/picas/Picerija.java
@@ -7,8 +7,11 @@ public class Picerija {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int telNumurs;
-		String vardsUzv, dzivvieta;
-		telNumurs = Integer.parseInt(JOptionPane.showInputDialog("Ieraksti telefona numuru!"));
+		String vardsUzv, dzivvieta, x;
+		do {
+		x = JOptionPane.showInputDialog("Ieraksti telefona numuru!");
+		}while(x.length() != 8);
+		telNumurs = Integer.parseInt(x);
 		vardsUzv = JOptionPane.showInputDialog("Ieraksti vārdu un uzvārdu!");
 		dzivvieta = JOptionPane.showInputDialog("Ieraksti adresi");
 		Pasutitajs sutitajs = new Pasutitajs(telNumurs, vardsUzv, dzivvieta);
