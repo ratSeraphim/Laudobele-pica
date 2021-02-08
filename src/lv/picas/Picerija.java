ab8f5ad424d25b4d60be041f063ba5a5b211f0f2
Telma Laudobele <telmy2458@hotmail.lv>
2021-02-08 19:02:59 +0200

    Pievienoju sākotnēju izvēli

--- src/lv/picas/Picerija.java
+++ src/lv/picas/Picerija.java
@@ -6,18 +6,33 @@ public class Picerija {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		   String[] options = {"Jauns sūtījums", "Aktīvie sūtījumi", "Iziet no programmas"};
+	      
+	        int x = JOptionPane.showOptionDialog(null, "Veic izvēli!",
+	                null,
+	                JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+	     switch(x) {
+	     case 0: jaunsSutijums();
+			
+	     case 1: JOptionPane.showMessageDialog(null, "Nav izstrādāts kods...");
+	    	 
+	     case 2: System.exit(0);
+default: 
+	    }
+		
+	}
+	public static void jaunsSutijums(){
 		int telNumurs;
-		String vardsUzv, dzivvieta, x;
+		String vardsUzv, dzivvieta, y;
 		do {
-		x = JOptionPane.showInputDialog("Ieraksti telefona numuru!");
-		}while(x.length() != 8);
-		telNumurs = Integer.parseInt(x);
+			y = JOptionPane.showInputDialog("Ieraksti telefona numuru!");
+		}while(y.length() != 8);
+		telNumurs = Integer.parseInt(y);
 		vardsUzv = JOptionPane.showInputDialog("Ieraksti vārdu un uzvārdu!");
 		dzivvieta = JOptionPane.showInputDialog("Ieraksti adresi");
 		Pasutitajs sutitajs = new Pasutitajs(telNumurs, vardsUzv, dzivvieta);
 		sutitajs.pasutitajaDati();
 		sutitajs.pasutitPicu();
-		
 	}
 
 }
