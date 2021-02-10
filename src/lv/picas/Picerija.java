c1b1e529375d8a2ae6f52e6a3a3d4b65912e88f4
Telma Laudobele <telmy2458@hotmail.lv>
2021-02-10 17:15:23 +0200

    Pievienoju lasīšanas funkcionalitāti

--- src/lv/picas/Picerija.java
+++ src/lv/picas/Picerija.java
@@ -1,9 +1,12 @@
 package lv.picas;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import javax.swing.JOptionPane;
 
 public class Picerija {
-	
+	static String teksts;
 	public static void main(String[] args) {
 		int x;
 		 do {  String[] options = {"Jauns sūtījums", "Aktīvie sūtījumi", "Iziet no programmas"};
@@ -15,7 +18,18 @@ public class Picerija {
 	     case 0: jaunsSutijums();
 	     break;
 	     
-	     case 1: JOptionPane.showMessageDialog(null, "Nav izstrādāts kods...");
+	     case 1: 
+	     try {
+	    	 FileReader fr= new FileReader("Pasutijumi.txt");
+				BufferedReader lasa = new BufferedReader(fr);
+				while((teksts = lasa.readLine())!=null) {
+					System.out.println(teksts);
+				}
+	      lasa.close();
+	     }catch(Exception e) {
+				JOptionPane.showMessageDialog(null, "Kļūme ierakstot faila!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
+			}
+	     
 	     break;
 	    	 
 	     case 2: JOptionPane.showMessageDialog(null,"Programma tiek apturēta","Brīdinājums", JOptionPane.WARNING_MESSAGE);
