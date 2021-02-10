fbc9251d0543e2e162ab0460fcf97be168fa0305
Telma Laudobele <telmy2458@hotmail.lv>
2021-02-10 18:41:35 +0200

    Pievienoju nodoto pasūtījumu failu, nolasīšanu.

--- src/lv/picas/Picerija.java
+++ src/lv/picas/Picerija.java
@@ -1,12 +1,9 @@
 package lv.picas;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import javax.swing.JOptionPane;
 
@@ -14,7 +11,7 @@ public class Picerija {
 	static String teksts;
 	public static void main(String[] args) {
 		int x;
-		 do {  String[] options = {"Jauns sūtījums", "Aktīvie sūtījumi", "Nodot visus sūtījumus", "Iziet no programmas"};
+		 do {  String[] options = {"Jauns sūtījums", "Skatīt aktīvos sūtījumi", "Nodot visus aktīvos sūtījumus", "Skatīt nodotos sūtījumus", "Iziet no programmas"};
 	      
 	        x = JOptionPane.showOptionDialog(null, "Veic izvēli!",
 	                null,
@@ -35,9 +32,11 @@ public class Picerija {
 				JOptionPane.showMessageDialog(null, "Kļūme ierakstot faila!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
 			}
 	     
+	     
 	     break;
 	    	 
 	     case 2: 
+	    	 nodot();
 	    	 try {
 	 			FileWriter fw = new FileWriter("Pasutijumi.txt");
 	 			PrintWriter raksta = new PrintWriter(fw);
@@ -49,11 +48,22 @@ public class Picerija {
 	 			JOptionPane.showMessageDialog(null, "Kļūme ierakstot failā!","Kļūme!", JOptionPane.ERROR_MESSAGE);
 	 		}
 	     break;
-	     case 3: JOptionPane.showMessageDialog(null,"Programma tiek apturēta","Brīdinājums", JOptionPane.WARNING_MESSAGE);
+	     case 3:  try {
+	    	 FileReader fr= new FileReader("NodotiePasutijumi.txt");
+				BufferedReader lasa = new BufferedReader(fr);
+				while((teksts = lasa.readLine())!=null) {
+					System.out.println(teksts);
+				}
+	      lasa.close();
+	     }catch(Exception e) {
+				JOptionPane.showMessageDialog(null, "Kļūme ierakstot faila!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
+			}
 	     break;
+	     case 4: JOptionPane.showMessageDialog(null,"Programma tiek apturēta","Brīdinājums", JOptionPane.WARNING_MESSAGE); break;
+	    	 
 default: JOptionPane.showMessageDialog(null,"Jūs neveicāt izvēli!","Brīdinājums", JOptionPane.ERROR_MESSAGE);
 	    }
-		 }while(x != 3);
+		 }while(x != 4);
 		
 	}
 	public static void jaunsSutijums(){
@@ -70,7 +80,27 @@ default: JOptionPane.showMessageDialog(null,"Jūs neveicāt izvēli!","Brīdinā
 		sutitajs.pasutitajaDati();
 		sutitajs.pasutitPicu();
 	}
-	
+	public static void nodot() {
+		try {
+	    	 FileReader fr= new FileReader("Pasutijumi.txt");
+				BufferedReader lasa = new BufferedReader(fr);
+				while((teksts = lasa.readLine())!=null) {
+					 try {
+				 			FileWriter fw = new FileWriter("NodotiePasutijumi.txt", true);
+				 			PrintWriter raksta = new PrintWriter(fw);
+				 			raksta.println(teksts);
+				 			raksta.close();
+				 					
+				 		}catch(Exception e) {
+				 			JOptionPane.showMessageDialog(null, "Kļūme ierakstot failā!","Kļūme!", JOptionPane.ERROR_MESSAGE);
+				 		}
+					 JOptionPane.showMessageDialog(null, "Fails saglabāts", "Uzmanību", JOptionPane.INFORMATION_MESSAGE);
+				}
+	      lasa.close();
+	     }catch(Exception e) {
+				JOptionPane.showMessageDialog(null, "Kļūme ierakstot faila!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
+			}
+	}
 
 }
 
